76085f63e8337c80e9096228e052cac8bd528caf
Hwan0518 <qip0518@naver.com>
2024-01-09 16:04:55 +0900

    infra: git actions test 2

--- src/main/java/gentledog/members/MembersApplication.java
+++ src/main/java/gentledog/members/MembersApplication.java
@@ -9,6 +9,7 @@ public class MembersApplication {
     public static void main(String[] args) {
         SpringApplication.run(MembersApplication.class, args);
         // git actions test
+        // git actions test 2
     }
 
 }
